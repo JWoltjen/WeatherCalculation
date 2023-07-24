40e29f567002b980fbcbd7586505b89137d042da
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-07-24 13:47:54 -0500

    Create temperature class with insert method and main method that loops through temperature class insert method to add 100 random temps

--- WeatherCalculations/Program.cs
+++ WeatherCalculations/Program.cs
@@ -4,24 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         var temperature = new Temperature();
         var random = new Random();
-        // loop over temperature method to insert 100 temps
-        for (int i = 0; i < 101; i++)
+
+        while (true)
         {
-            // the random insertion should be between 1 and 100
-            temperature.Insert(random.Next(1, 101));
+            Console.WriteLine("Press 'q' to quit, any other key to continue");
+            var input = Console.ReadKey();
+            if(input.Key == ConsoleKey.Q)
+            {
+                break;
+            }
+            Console.WriteLine();
+            // loop over temperature method to insert 100 temps
+            for (int i = 0; i < 101; i++)
+            {
+                // the random insertion should be between 1 and 100
+                temperature.Insert(random.Next(1, 101));
+            }
+            // output the min, max, and average values to the console
+            Console.WriteLine($"Min temperature is {temperature.Min}");
+            Console.WriteLine($"Max temperature is {temperature.Max}");
+            Console.WriteLine($"Average temperature is {temperature.Average:F2}");
         }
-        // output the min, max, and average values to the console
-        Console.WriteLine($"Min temperature is {temperature.Min}");
-        Console.WriteLine($"Max temperature is {temperature.Max}");
-        Console.WriteLine($"Average temperature is {temperature.Average}");
     }
 }
 
 class Temperature
 {
-    // create properties for min, max, and average
+    // create properties for min, max, average, sum, and count
     public int Min { get; private set; } = int.MaxValue;
     public int Max { get; private set; } = int.MinValue;
     public double Average => Sum / (double)Count;
@@ -31,7 +43,7 @@ class Temperature
    // create a method to insert random temperatures from 1 to 100
    public void Insert(int temperature)
     {
-        if(temperature< Min)
+        if(temperature < Min)
         {
             Min = temperature;
         }
